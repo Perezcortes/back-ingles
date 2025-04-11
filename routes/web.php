d73c16d2e77183a8c1cf0da83d404399470752d2
Jonatan Eduardo Ramirez Lopez <jonatanrammirez@gmail.com>
2025-04-11 03:08:43 -0600

    Ultimo commit de la estructura base de la API.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 // Controladores necesarios
 require_once 'controllers/LevelController.php';
 
-// Rutas para Level con parámetros dinámicos
+// Rutas para Level con parámetros dinámicos 
 Route::get('/levels/getAll', [LevelController::class, 'getAll']);
 Route::get('/levels/getOne/{id}', [LevelController::class, 'getOne']);
 Route::post('/levels/create', [LevelController::class, 'create']);
